5e397e1269277a7978e3c0471c210fcac1002ef5
Brandon Seymour <bseymour122@yahoo.ca>
2022-06-05 15:59:29 -0400

    Add game module
    This module will control the flow of the game. Several functions were
    added to the module. The switchTurn function will switch the turns of
    the players after a move has been made. The isWin function will check
    if a player has won the game and the isTie function will check if there
    has been a tie.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,13 +1,14 @@
 const gameboard = (function () { // Module for the gameboard
-    const _board = ['','','','','','','','','']; // Empty board array
+    const board = ['','','','','','','','','']; // Empty board array
 
     /* Adds moves to the gameboard using an array for moves and a symbol to indicate the player */
-    function _addToBoard(player, moves) {
+    function addToBoard(player, moves) {
         for (let index = 0; index < moves.length; index++) {
             let move = moves[index];
-            _board[move] = player;
+            board[move] = player;
         }
     }
+    return {board, addToBoard}
 })();
 
 const Player = function (name, symbol) { // Factory function to create player objects
@@ -72,3 +73,96 @@ const displayController = (function () { // Module to display gameboard
     }
 return {displayBoard, clearBoard, getPlayerObj};
 })();
+
+const game = (function () { // Module to control the flow of the game
+    const player1 = Player('Player 1', 'x');
+    const player2 = Player('Player 2', 'o');
+    
+    let player1Symbol = player1.getSymbol();
+    let player2Symbol = player2.getSymbol();
+
+    let board = gameboard.board;
+    
+    function switchTurn(playerTurn) {
+        if (playerTurn === 'x') {
+            return player2Symbol;
+        } else if (playerTurn === 'o') {
+            return player1Symbol;
+        } else {
+            return
+        }
+    }
+
+    function isWin(board, symbol) {
+        const n = 3; // size of rows and columns
+        
+        // Checking rows
+        for (let i = 0; i < n; i++) {
+            let win = true;
+            for (let j = 0; j < n; j++) {
+                const element = (n*i) + j;
+                if (board[element] !== symbol) {
+                    win = false;
+                    break;
+                }
+            } 
+            if (win) {
+                return win;
+            }
+        }
+
+        // Checking columns
+        for (let i = 0; i < n; i++) {
+            let win = true;
+            for (let j = 0; j < n; j++) {
+                const element = (n*j) + i;
+                if (board[element] !== symbol) {
+                    win = false;
+                    break;
+                }  
+            }
+            if (win) {
+                return win;
+            }
+        }
+
+        // Checking diagonals
+        let win = true;
+        for (let i = 0; i < n; i++) {
+            const element = (n + 1)*(i);
+            if (board[element] !== symbol) {
+                win = false;
+                break;
+            }
+        }
+        if (win) {
+            return win;
+        }
+
+        win = true;
+        for (let i = 0; i < n; i++) {
+            const element = (n - 1)*(1 + i);
+            if (board[element] !== symbol) {
+                win = false;
+                break;
+            }
+        }
+        if (win) {
+            return win;
+        }
+        return win;
+    }
+    
+    function isTie(board) {
+        let tie = true;
+        for (let i = 0; i < board.length; i++) {
+            const element = board[i];
+            if (element === '') {
+                tie = false;
+            }
+        }
+        return tie;
+    }
+
+    return {player1, player2, switchTurn, isWin, isTie, board};
+ })();
